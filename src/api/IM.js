423c346e3a254467b139052b0a4b3dc6ce3942c5
peilun fu-laptop <peilun.fu117@gmail.com>
2019-03-15 18:14:15 +0800

    initialization

---
+++ src/api/IM.js
@@ -0,0 +1,17 @@
+import request from '@/utils/request'
+
+export function get_statunum(query) {
+  return request({
+    url: '/imstatus/',
+    method: 'get',
+    params: query
+  })
+}
+
+export function get_trend(query) {
+  return request({
+    url: '/trend/',
+    method: 'get',
+    params: query
+  })
+}
